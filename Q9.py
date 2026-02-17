ae66f10e30ba8896ab8ddc3e10af4c43d32e4fd4
janshikuramana-hub <janshikuramana@gmail.com>
2026-02-17 20:28:10 +0530

    Q9.py

--- Q9.py
+++ Q9.py
@@ -1,1 +1,8 @@
-team name-{pycharmers}
+#team name-{pycharmers}
+def generate_threes(start: int, end: int) -> list[int]:
+    """
+    Generate a list of numbers from start to end, skipping by 3.
+    """
+    if start >= end:
+        return []
+    return list(range(start, end, 3))
