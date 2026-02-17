17c3864de9253950c42ace1b19c4da06584b2737
janshikuramana-hub <janshikuramana@gmail.com>
2026-02-17 20:29:10 +0530

    Q10.py

--- Q10.py
+++ Q10.py
@@ -1,1 +1,6 @@
-team name-{pycharmers}
+#team name-{pycharmers}
+def organize_scores(scores: list[int], descending: bool) -> list[int]:
+    """
+    Sort scores without modifying the original list.
+    """
+    return sorted(scores, reverse=descending)
